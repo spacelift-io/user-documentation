0bc9d9f9ee77a3ca8a75262c6448739646888369
Roman Grinovski <roman.grinovski@gmail.com>
2023-05-24 22:44:45 +0200

    wire-up env var

--- hooks/fetch_banner.py
+++ hooks/fetch_banner.py
@@ -1,12 +1,12 @@
 import requests
-import os
 
 def on_config(config):
     wp_options_api_url = config['extra']['wp_options_api_url']
-    response = requests.get(wp_options_api_url)
-    data = response.json()
-    banner = data['acf']['hello_banner']
 
-    config['extra'].update({'banner': banner})
+    if(wp_options_api_url):
+        response = requests.get(wp_options_api_url)
+        data = response.json()
+        banner = data['acf']['hello_banner']
+        config['extra'].update({'banner': banner})
 
     return config
