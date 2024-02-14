aad3639be1c1f02cb506e30d0208211b2a775b4a
Krzysztof Niepokojczycki <krzysztofn@spacelift.io>
2024-02-14 12:26:48 +0100

    chore: remove user management docs from exclude script

--- scripts/remove-files-based-on-distribution.sh
+++ scripts/remove-files-based-on-distribution.sh
@@ -4,7 +4,6 @@ if [[ "$SPACELIFT_DISTRIBUTION" == "SELF_HOSTED" ]]; then
   # Remove any SaaS-only pages
   rm docs/self-hosted.md
   rm docs/concepts/vcs-agent-pools.md
-  rm -rf docs/concepts/user-management
   rm docs/integrations/cloud-providers/azure.md
   rm docs/integrations/cloud-providers/gcp.md
   rm docs/product/billing/aws-marketplace.md
