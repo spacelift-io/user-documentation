4e005c01f7415b0f0473f8cc7392541149dca4fa
apollorion <joey@apollorion.com>
2024-06-21 19:11:47 -0400

    Revert "feat: move files so that we dont break links on accident"
    This reverts commit 0644d0a5b2db6db78dddf21ac5b8ee8eabf5f1a1.

--- hooks/move_index.py
+++
@@ -1,9 +0,0 @@
-from mkdocs.structure.files import Files
-from mkdocs.config.defaults import MkDocsConfig
-
-
-def on_files(files: Files, config: MkDocsConfig):
-    for file in files:
-        if file.dest_uri.endswith("index.html") and file.dest_uri != "index.html":
-            file.dest_uri = file.dest_uri.replace("/index.html", ".html")
-    return files
